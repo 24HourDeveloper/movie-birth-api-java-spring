7ab32c1c8d70c79def049230c8eb74d6ba8a7d51
unknown <mikeitt009@gmail.com>
2020-01-12 14:42:25 -0500

    first commit only MovieBirthApiApplication.java file

---
+++ src/main/java/com/uncodedlifestyle/moviebirthapi/MovieBirthApiApplication.java
@@ -0,0 +1,13 @@
+package com.uncodedlifestyle.moviebirthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBirthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBirthApiApplication.class, args);
+	}
+
+}
