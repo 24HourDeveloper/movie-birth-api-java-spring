e547e8ff35ef2078ad36c3399cceca335c997ee4
unknown <mikeitt009@gmail.com>
2020-01-13 10:10:31 -0500

    created Movies class and implemented restTemplate to parse movies api and send data to /movie endpoint

--- src/main/java/com/uncodedlifestyle/moviebirthapi/controllers/MovieController.java
+++ src/main/java/com/uncodedlifestyle/moviebirthapi/controllers/MovieController.java
@@ -2,22 +2,31 @@ package com.uncodedlifestyle.moviebirthapi.controllers;
 
 import com.uncodedlifestyle.moviebirthapi.models.MovieModel;
 import com.uncodedlifestyle.moviebirthapi.models.Movies;
+import com.uncodedlifestyle.moviebirthapi.secure.Secure;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class MovieController {
 
-    private String url = "https://api.themoviedb.org/3/discover/movie?api_key=1ad631084611d4fcc3f954f51d4378c7&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
-    private RestTemplate restTemplate;
+    private Secure secure = new Secure();
 
-    @RequestMapping("/api/movie")
-    public List<Movies> getMovieBirth(){
+    @RequestMapping("/movie")
+    public List<Movies> getMovieBirth(@RequestParam("birth") String birth) throws Exception{
+        RestTemplate restTemplate;
+        String release_year = "&primary_release_year=";
+        String url = "https://api.themoviedb.org/3/discover/movie?api_key=" + secure.getAPI() + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+
         restTemplate = new RestTemplate();
-        MovieModel movies = restTemplate.getForObject(url, MovieModel.class);
+        MovieModel movies = restTemplate.getForObject(url + release_year + birth, MovieModel.class);
         return  movies.getResults();
     }
+
 }
